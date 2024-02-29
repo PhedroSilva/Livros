a819f522ceb6a55ebf765c5ba4756ff937ce1850
PhedroSilva <133990374+PhedroSilva@users.noreply.github.com>
2024-02-29 13:16:51 -0300

    Add files via upload

---
+++ prjDTO/src/main/java/com/example/demo/service/LivroService.java
@@ -0,0 +1,58 @@
+package com.example.demo.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.dto.LivroDTO;
+import com.example.demo.entities.Livro;
+import com.example.demo.repository.LivroRepository;
+
+@Service
+public class LivroService {
+	private final LivroRepository livroRepository;
+
+	@Autowired
+	public LivroService(LivroRepository livroRepository) {
+		this.livroRepository = livroRepository;
+	}
+
+	// serviço de busca de todos os registros
+	public List<Livro> buscaTodos() {
+		return livroRepository.findAll();
+	}
+
+	// serviço de busca de registros por id
+	public Livro buscaPorId(Long id) {
+		return livroRepository.findById(id).orElse(null);
+	}
+
+	// serviço de salvar livro
+	public LivroDTO salvar(Livro livro) {
+		Livro salvarLivro = livroRepository.save(livro);
+		return new LivroDTO(salvarLivro.getId(), salvarLivro.getTitulo(), salvarLivro.getAutor());
+	}
+
+	// serviço de atualizar livro
+	public LivroDTO atualizar(Long id, Livro livro) {
+		Livro exiteLivro = livroRepository.findById(id)
+				.orElseThrow(() -> new RuntimeException("livro" + "não encontrado"));
+		exiteLivro.setTitulo(livro.getTitulo());
+		exiteLivro.setTitulo(livro.getAutor());
+		Livro updateLivro = livroRepository.save(exiteLivro);
+		return new LivroDTO(updateLivro.getId(), updateLivro.getTitulo(), updateLivro.getAutor());
+	}
+
+	// serviço de deletar livro
+	public boolean deletar(Long id) {
+		Optional<Livro> exiteLivro = livroRepository.findById(id);
+		if (exiteLivro.isPresent()) {
+			livroRepository.deleteById(id);
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
